29c677ee2f7bd111985747b5f74ca0cd079c9fac
neha1053 <nehaec1053@gmail.com>
2016-09-29 14:39:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ alert('hi');
 var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginLeft  = '100px';
 };
